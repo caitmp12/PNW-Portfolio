1eacc6b4f6f3e96f9fe4492f89bf25fd4d4315b5
Caitlin Moore-Penaskovic <cait.moorepenaskovic@gmail.com>
2021-04-07 20:43:31 -0400

    blog component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Nav from "./components/Nav"
 import Carousel from "./components/Carousel"
+import Blog from "./components/Blog"
 
 function App() {
   return (
