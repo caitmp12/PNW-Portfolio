7965bcc77cfbdc695721c15aa20d29c582922581
Caitlin Moore-Penaskovic <cait.moorepenaskovic@gmail.com>
2021-04-07 20:36:59 -0400

    set up components, carousel and nav

---
+++ src/components/Carousel.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { LInk } from "react-router-dom"
+
+const Carousel = (props) => {
+    return (
+        <div>
+            <h1>ooooh pictures</h1>
+        </div>
+    )
+}
+
+export default Carousel;
---
+++ src/components/Nav.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { LInk } from "react-router-dom"
+
+const Nav = (props) => {
+    return (
+        <div> 
+            <h1>This is the Nav!</h1>
+        </div>
+    )
+}
+
+export default Nav;
