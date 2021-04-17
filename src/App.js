319c7e3ede8e82a9221a7ad2447f73dbb36a0cda
Caitlin Moore-Penaskovic <cait.moorepenaskovic@gmail.com>
2021-04-17 12:34:39 -0400

    Footer component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Nav from "./components/Nav"
 import Home from "./components/Home"
+import Footer from "./components/Footer"
 import Blog from "./components/Blog"
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <div>
       <Nav />
       <Home />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { LInk } from "react-router-dom"
+
+const Footer = (props) => {
+    return (
+        <>
+            <div className="footer-box">
+                <h1>PNW Photography LLC</h1>
+            </div>
+        </>
+    )
+}
+
+export default Footer;
