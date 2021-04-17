f85ca0ed29cf3c32c6c56a5fe715de49b5c84038
Caitlin Moore-Penaskovic <cait.moorepenaskovic@gmail.com>
2021-04-17 12:32:16 -0400

    home html

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,9 +3,41 @@ import { LInk } from "react-router-dom"
 
 const Home = (props) => {
     return (
-        <div>
-            <h1>ooooh pictures</h1>
+        <>
+        {/* Carousel */}
+        <div className="carousel">
+            <div className="image">
+                <div className="carousel-title">
+                    <h1>Pacific North West Photography</h1>
+                </div>
+                <div className="gallery-prompt">
+                    <div className="gallery-text">
+                        <h2>See More</h2>
+                    </div>
+                    <div className="gallery-image">
+                        <h2>#</h2>
+                    </div>
+                </div>
+            </div>
+        </div>  
+
+        {/* About */}
+        <div className="about">
+            <div className="about-image">
+                <h3>picture of Brendan</h3>
+            </div>
+            <div className="about-text">
+                <h1>About PNW</h1>
+                <p>Brendan and Ashley are super wonderful and take amazing pictures and can you tell this is all a placeholder?</p>
+            </div>
         </div>
+
+        {/* Contact Me */}
+        <div className="contact">
+            <h1>Contact Me</h1>
+        </div>
+
+        </>
     )
 }
 
