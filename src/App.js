c0aa3d9738feddaea19038a4814300afae776a9f
Caitlin Moore-Penaskovic <cait.moorepenaskovic@gmail.com>
2021-04-17 11:53:23 -0400

    html for Nav

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Nav from "./components/Nav"
-import Carousel from "./components/Home"
+import Home from "./components/Home"
 import Blog from "./components/Blog"
 
 function App() {
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,9 +3,21 @@ import { LInk } from "react-router-dom"
 
 const Nav = (props) => {
     return (
-        <div> 
-            <h1>This is the Nav!</h1>
+        <> 
+        <div className="nav-box">
+            <div className="nav-logo">
+                <h1>PNW</h1>
+            </div>
+            <div className="nav">
+                <ul>
+                    <li>About</li>
+                    <li>Gallery</li>
+                    <li>Blog</li>
+                    <li>Contact</li>    
+                </ul>
+            </div>
         </div>
+        </>
     )
 }
 
