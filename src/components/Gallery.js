fdd6bd86fe0c972df3a2b2390eabac618ffb6d1e
Caitlin Moore-Penaskovic <cait.moorepenaskovic@gmail.com>
2021-04-24 10:55:42 -0400

    gallery notes

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -5,6 +5,7 @@ const Gallery = (props) => {
     return (
         <div>
             <h1>Comming Soon</h1>
+            <div>Filter goes here</div>
             <div className="gallery-wall">
                 <div>Image placeholder</div>
             </div>
