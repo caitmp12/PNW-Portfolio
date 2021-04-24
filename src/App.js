166ad83ac286978e2cc3b92a01be1fdb05c8af1e
Caitlin Moore-Penaskovic <cait.moorepenaskovic@gmail.com>
2021-04-24 10:50:41 -0400

    css wireframing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Link, Switch } from "react-router-dom"
 import Nav from "./components/Nav"
 import Home from "./components/Home"
 import Footer from "./components/Footer"
